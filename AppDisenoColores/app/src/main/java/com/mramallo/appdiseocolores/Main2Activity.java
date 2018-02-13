308048a5e345d16db59a1d8f17ba6a71e7595315
ManuelRamallo <mramallodiaz96@gmail.com>
2018-02-13 20:08:32 +0100

    cambio en los botones

--- AppDisenoColores/app/src/main/java/com/mramallo/appdiseocolores/Main2Activity.java
+++ AppDisenoColores/app/src/main/java/com/mramallo/appdiseocolores/Main2Activity.java
@@ -1,13 +1,30 @@
 package com.mramallo.appdiseocolores;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        button = findViewById(R.id.buttonVolver);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(in);
+            }
+        });
+
     }
 }
