ee579aae41e67c733da46c390cbf8896f7f40895
ManuelRamallo <mramallodiaz96@gmail.com>
2018-02-21 19:53:16 +0100

    Se consigue actualización de la posición con la ubicación y refresco al bajar la página para actualizar su contenido

--- ProTGT/app/src/main/java/com/joandma/protgt/Activities/HomeActivity.java
+++ ProTGT/app/src/main/java/com/joandma/protgt/Activities/HomeActivity.java
@@ -1,33 +1,49 @@
 package com.joandma.protgt.Activities;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.support.annotation.NonNull;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.joandma.protgt.API.ServiceGenerator;
 import com.joandma.protgt.R;
 
-public class HomeActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks{
+public class HomeActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, SwipeRefreshLayout.OnRefreshListener, LocationListener{
 
-    private static final int PETICION_PERMISO_LOCALIZACION = 200;
+    private static final String LOGTAG = "android-localizacion";
+    private static final int PETICION_PERMISO_LOCALIZACION = 101;
+    private static final int PETICION_CONFIG_UBICACION = 201;
+
+
+    //TODO Falta actualizar la imagen cuando pinches en ella y darle accion
+    //TODO Falta darle una accion al boton del telefono para el intent inplicito de la llamada
     TextView localizacion;
+    SwipeRefreshLayout swipeContainer;
+    LocationRequest locRequest;
 
-    //TODO Esto falla, hay que seguir haciendo comprobaciones
-    GoogleApiClient apiClient = new GoogleApiClient.Builder(this)
-            .enableAutoManage(this, this)
-            .addConnectionCallbacks(this)
-            .addApi(LocationServices.API)
-            .build();
+    private GoogleApiClient apiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +52,131 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.O
 
         localizacion = findViewById(R.id.textViewLocalizacion);
 
+        //Construccion cliente API Google
+        apiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
 
 
+        //RefreshLayout para actualizar deslizando la pantalla hacia abajo
+        swipeContainer = findViewById(R.id.contenedor);
+        swipeContainer.setOnRefreshListener(this);
+        //Esto sirve para cambiarle los colores al circulo que va dando vueltas
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
 
+    private void toggleLocationUpdates(boolean enable) {
+        if (enable) {
+            enableLocationUpdates();
+        } else {
+            disableLocationUpdates();
+        }
+    }
+
+    private void enableLocationUpdates(){
+
+        locRequest = new LocationRequest();
+        locRequest.setInterval(2000);
+        locRequest.setFastestInterval(1000);
+        locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest locSettingsRequest =
+                new LocationSettingsRequest.Builder()
+                        .addLocationRequest(locRequest)
+                        .build();
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        apiClient, locSettingsRequest);
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult locationSettingsResult) {
+                final Status status = locationSettingsResult.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+
+                        Log.i(LOGTAG, "Configuración correcta");
+                        startLocationUpdates();
+
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            Log.i(LOGTAG, "Se requiere actuación del usuario");
+                            status.startResolutionForResult(HomeActivity.this, PETICION_CONFIG_UBICACION);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.i(LOGTAG, "Error al intentar solucionar configuración de ubicación");
+                        }
+
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.i(LOGTAG, "No se puede cumplir la configuración de ubicación necesaria");
+                        break;
+                }
+            }
+        });
+
+    }
+
+    private void disableLocationUpdates(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                apiClient,  HomeActivity.this);
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(HomeActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            //Ojo: estamos suponiendo que ya tenemos concedido el permiso.
+            //Sería recomendable implementar la posible petición en caso de no tenerlo.
+
+            Log.i(LOGTAG, "Inicio de recepción de ubicaciones");
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    apiClient, locRequest, HomeActivity.this);
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        //Se ha producido un error que no se puede resolver automáticamente
+        //y la conexión con los Google Play Services no se ha establecido.
+        Log.e(LOGTAG, "Error grave al conectar con Google Play Services");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        //Conectado correctamente a Google Play Services
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PETICION_PERMISO_LOCALIZACION);
+        } else {
+
+            Location lastLocation =
+                    LocationServices.FusedLocationApi.getLastLocation(apiClient);
+
+            updateUI(lastLocation);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //Se ha interrumpido la conexión con Google Play Services
+
+        Log.e(LOGTAG, "Se ha interrumpido la conexión con Google Play Services");
+    }
 
     private void updateUI(Location loc){
         if(loc != null){
@@ -51,30 +187,7 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.O
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        //Se ha producido un error que no se puede resolver automáticamente
-        //y la conexión con los Google Play Services no se ha establecido.
-        Log.e("LOGTAG", "Error grave al conectar con Google Play Services");
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PETICION_PERMISO_LOCALIZACION);
-        } else {
-            Location lastLocation =
-                    LocationServices.FusedLocationApi.getLastLocation(apiClient);
-
-            updateUI(lastLocation);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == PETICION_PERMISO_LOCALIZACION) {
             if (grantResults.length == 1
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -91,15 +204,59 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.O
                 //Permiso denegado:
                 //Deberíamos deshabilitar toda la funcionalidad relativa a la localización.
 
-                Log.e("LOGTAG", "Permiso denegado");
+                Log.e(LOGTAG, "Permiso denegado");
             }
         }
     }
 
-    @Override
-    public void onConnectionSuspended(int i) {
-        //Se ha interrumpido la conexión con Google Play Services
 
-        Log.e("LOGTAG", "Se ha interrumpido la conexión con Google Play Services");
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PETICION_CONFIG_UBICACION:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        startLocationUpdates();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i(LOGTAG, "El usuario no ha realizado los cambios de configuración necesarios");
+                        break;
+                }
+                break;
+        }
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(LOGTAG, "Recibida nueva ubicación!");
+
+        //Mostramos la nueva ubicación recibida
+        updateUI(location);
+    }
+
+
+    //Esto es del refreshLayout para actualizar solo con bajar el activity
+    //TODO Esto tenemos que probarlo con el movil de jorge caminando por la calle para comprobar si está cogiendo de verdad la localización o no
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                 boolean actualizar = true;
+
+                if(actualizar == true){
+                    toggleLocationUpdates(actualizar);
+                    actualizar = false;
+                } else {
+                    toggleLocationUpdates(actualizar);
+                    actualizar = true;
+                }
+
+                Toast.makeText(HomeActivity.this, "Tu ubicación se ha actualizado", Toast.LENGTH_SHORT).show();
+                swipeContainer.setRefreshing(false);
+            }
+        }, 2000);
+    }
+
+
 }
