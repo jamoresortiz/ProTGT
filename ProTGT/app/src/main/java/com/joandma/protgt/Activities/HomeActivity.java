6972dc6c82d274c4537b92352ada6b3c14226bfe
ManuelRamallo <mramallodiaz96@gmail.com>
2018-02-23 12:24:22 +0100

    agregado última librería, y reformado el HomeActivity para permisos, terminado, falta retrofit

--- ProTGT/app/src/main/java/com/joandma/protgt/Activities/HomeActivity.java
+++ ProTGT/app/src/main/java/com/joandma/protgt/Activities/HomeActivity.java
@@ -1,13 +1,16 @@
 package com.joandma.protgt.Activities;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -22,6 +25,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.joandma.protgt.R;
 
 import java.util.ArrayList;
@@ -34,11 +48,17 @@ import io.nlopez.smartlocation.location.config.LocationParams;
 import io.nlopez.smartlocation.location.providers.LocationGooglePlayServicesProvider;
 
 public class HomeActivity extends AppCompatActivity
-        implements SwipeRefreshLayout.OnRefreshListener, OnLocationUpdatedListener {
+        implements SwipeRefreshLayout.OnRefreshListener, LocationListener, GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks {
 
-    private static final int LOCATION_PERMISSION_ID = 1001;
-    private LocationGooglePlayServicesProvider provider;
+    private static final String LOGTAG = "android-localizacion";
+    private static final int PETICION_PERMISO_LOCALIZACION = 101;
+    private static final int PETICION_CONFIG_UBICACION = 201;
+
     TextView localizacion;
+    LocationRequest locRequest;
+    private GoogleApiClient apiClient;
+
 
     //TODO Falta darle retrofit a la imagen para que mande los datos
     SwipeRefreshLayout swipeContainer;
@@ -55,8 +75,6 @@ public class HomeActivity extends AppCompatActivity
         imagenEmergencia = findViewById(R.id.imageViewEmergencia);
         imagenLlamada = findViewById(R.id.imageViewLlamada);
 
-        startLocation();
-
         imagenEmergencia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,6 +88,13 @@ public class HomeActivity extends AppCompatActivity
             }
         });
 
+        //Construccion cliente API Google
+        apiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+
 
         imagenLlamada.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,25 +102,6 @@ public class HomeActivity extends AppCompatActivity
                 dialPhoneNumber();
             }
         });
-
-
-        //TODO PERMISOS
-       /* int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-
-            } else {
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        LOCATION_PERMISSION_ID);
-
-            }
-        }*/
 
 
         //RefreshLayout para actualizar deslizando la pantalla hacia abajo
@@ -144,72 +150,128 @@ public class HomeActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    private void toggleLocationUpdates(boolean enable) {
+        if (enable) {
+            enableLocationUpdates();
+        } else {
+            disableLocationUpdates();
+        }
+    }
 
+    private void enableLocationUpdates(){
 
-    //Este metodo es donde se piden los permisos de lo que vayamos a usar
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-       /* switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length >
-                        && grantResults[] == PackageManager.PERMISSION_GRANTED) {
+        locRequest = new LocationRequest();
+        locRequest.setInterval(2000);
+        locRequest.setFastestInterval(1000);
+        locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-                } else {
+        LocationSettingsRequest locSettingsRequest =
+                new LocationSettingsRequest.Builder()
+                        .addLocationRequest(locRequest)
+                        .build();
 
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        apiClient, locSettingsRequest);
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult locationSettingsResult) {
+                final Status status = locationSettingsResult.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+
+                        Log.i(LOGTAG, "Configuración correcta");
+                        startLocationUpdates();
+
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            Log.i(LOGTAG, "Se requiere actuación del usuario");
+                            status.startResolutionForResult(HomeActivity.this, PETICION_CONFIG_UBICACION);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.i(LOGTAG, "Error al intentar solucionar configuración de ubicación");
+                        }
+
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.i(LOGTAG, "No se puede cumplir la configuración de ubicación necesaria");
+                        break;
                 }
-                return;
             }
-        }*/
-        if (requestCode == LOCATION_PERMISSION_ID && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            startLocation();
+        });
+
+    }
+
+    private void disableLocationUpdates(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                apiClient,  HomeActivity.this);
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(HomeActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            //Ojo: estamos suponiendo que ya tenemos concedido el permiso.
+            //Sería recomendable implementar la posible petición en caso de no tenerlo.
+
+            Log.i(LOGTAG, "Inicio de recepción de ubicaciones");
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    apiClient, locRequest, HomeActivity.this);
         }
     }
 
-    //Muestra la ultima localización, esto hay que setearlo en el textView del XML
-    private void showLast() {
-        Location lastLocation = SmartLocation.with(this).location().getLastLocation();
-        if (lastLocation != null) {
-            localizacion.setText(
-                    String.format("[Caché] Latitude %.6f, Longitude %.6f",
-                            lastLocation.getLatitude(),
-                            lastLocation.getLongitude())
-            );
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        //Se ha producido un error que no se puede resolver automáticamente
+        //y la conexión con los Google Play Services no se ha establecido.
+        Log.e(LOGTAG, "Error grave al conectar con Google Play Services");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        //Conectado correctamente a Google Play Services
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PETICION_PERMISO_LOCALIZACION);
+        } else {
+
+            Location lastLocation =
+                    LocationServices.FusedLocationApi.getLastLocation(apiClient);
+
+            updateUI(lastLocation);
         }
     }
 
-    //Empieza a buscar la localización
-    private void startLocation() {
+    @Override
+    public void onConnectionSuspended(int i) {
+        //Se ha interrumpido la conexión con Google Play Services
 
-        provider = new LocationGooglePlayServicesProvider();
-        provider.setCheckLocationSettings(true);
-        localizacion.setText("Active la ubicación o deslize para actualizar su posición");
-
-        SmartLocation smartLocation = new SmartLocation.Builder(this).logging(true).build();
-
-
-        // NAVIGATION cambia cada 0 metros
-        smartLocation.location().config(LocationParams.NAVIGATION).start(this);
-
+        Log.e(LOGTAG, "Se ha interrumpido la conexión con Google Play Services");
     }
 
-    //Muestra la localizacion
-    private void showLocation(Location location) {
-        if (location != null) {
+    private void updateUI(Location loc){
+        if(loc != null){
             final String text = String.format("",
-                    location.getLatitude(),
-                    location.getLongitude());
+                    loc.getLatitude(),
+                    loc.getLongitude());
 
             final String text2 = text;
 
-            if (text != text2){
+            if(text != text2){
                 localizacion.setText(text);
             }
 
 
-
             // We are going to get the address for the current position
-            SmartLocation.with(this).geocoding().reverse(location, new OnReverseGeocodingListener() {
+            SmartLocation.with(this).geocoding().reverse(loc, new OnReverseGeocodingListener() {
                 @Override
                 public void onAddressResolved(Location original, List<Address> results) {
                     if (results.size() > 0) {
@@ -226,28 +288,56 @@ public class HomeActivity extends AppCompatActivity
                 }
             });
         } else {
-            localizacion.setText("No hay localización");
+            localizacion.setText("Active la ubicación o deslize para actualizar");
         }
     }
 
-    //Para la localización
-    private void stopLocation() {
-        SmartLocation.with(this).location().stop();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PETICION_PERMISO_LOCALIZACION) {
+            if (grantResults.length == 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                //Permiso concedido
+
+                @SuppressWarnings("MissingPermission")
+                Location lastLocation =
+                        LocationServices.FusedLocationApi.getLastLocation(apiClient);
+
+                updateUI(lastLocation);
+
+            } else {
+                //Permiso denegado:
+                //Deberíamos deshabilitar toda la funcionalidad relativa a la localización.
+
+                Log.e(LOGTAG, "Permiso denegado");
+            }
+        }
     }
 
 
     @Override
-    public void onLocationUpdated(Location location) {
-        showLocation(location);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PETICION_CONFIG_UBICACION:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        startLocationUpdates();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i(LOGTAG, "El usuario no ha realizado los cambios de configuración necesarios");
+                        break;
+                }
+                break;
+        }
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(LOGTAG, "Recibida nueva ubicación!");
 
-    private void toggleLocationUpdates(boolean enable) {
-        if (enable) {
-            startLocation();
-        } else {
-            stopLocation();
-        }
+        //Mostramos la nueva ubicación recibida
+        updateUI(location);
     }
 
 
@@ -260,7 +350,7 @@ public class HomeActivity extends AppCompatActivity
             public void run() {
                 boolean actualizar = true;
 
-                if (actualizar == true) {
+                if(actualizar == true){
                     toggleLocationUpdates(actualizar);
                     actualizar = false;
                 } else {
@@ -272,7 +362,5 @@ public class HomeActivity extends AppCompatActivity
             }
         }, 2000);
     }
-
-
 
 }
